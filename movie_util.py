bd8ea175714c6d60a24025c9a02c3451e5697a08
Emil Stenström <em@kth.se>
2012-12-31 14:38:03 +0100

    Break out movie name parsing to a separate module.

---
+++ movie_util.py
@@ -0,0 +1,51 @@
+import re
+
+# Cut out the movie name from a torrent name
+def strip_title(title):
+    # Strings that mark the end of a movie name, and start of meta data
+    ends = [
+        "1080p", "720p", "bluray", "bdrip", "brrip", "hd",
+        "korsub", "extended", "uncut", "unrated", "repack",
+        "swesub", "ac3", "hdrip", "xvid", "dvdscr", "rc",
+        "hq", "boxset", "x264", "tc", "bdrip720p", "bdrip1080p",
+        "edition"
+    ]
+
+    # Remove all non-alpha characters
+    words = re.split(r"[\W_]+", title.lower())
+
+    # Loop over all words. As soon as a ending is found, cut to there
+    for i, word in enumerate(words):
+        if word in ends:
+            words = words[:i]
+            break
+    title = " ".join(words).lower()
+
+    # Remove unnessesary whitespace
+    return title.strip()
+
+# Reduce the number of duplicate movies by removing the year from name
+def unique(movies):
+    return set([("".join(re.split(" \d{4}$", title)).strip()) for title in movies])
+
+# Identify bundles of movies, as opposed to a single movie
+def is_bundle(title):
+    # Strings that identify a bundle
+    bundle_identifier = [
+        "trilogy", "duology", "quadrilogy",
+        "movies", "collection", "series", "complete",
+        " 1 3 ", " 1 4 ", " 1 5 "
+    ]
+    for identifier in bundle_identifier:
+        if identifier in title:
+            return True
+    return False
+
+# Separate movies, and movie bundles. Bundles will not be graded.
+def separate_movies_from_bundles(mixed):
+    movies = [movie for movie in mixed if not is_bundle(movie)]
+    bundles = [movie for movie in mixed if is_bundle(movie)]
+    return movies, bundles
+
+def filenames_to_search_strings(names):
+    return unique([strip_title(title) for url, title in names])
--- parse.py
+++ parse.py
@@ -1,18 +1,19 @@
 from __future__ import print_function
 import sys
-import re
 import os
 import hashlib
 import requests
 import json
-import codecs
 from datetime import datetime
+import codecs
 from time import sleep
 from lxml import html
 from urllib import urlencode
 from access_key import ACCESS_KEY, USER_KEY
+from movie_util import filenames_to_search_strings, separate_movies_from_bundles
 
-DEBUG = False # Can be set with --verbose
+DEBUG = False  # Can be set with "parse.py --verbose"
+PAGES = 10  # Can be set with "parse.py 5"
 
 # Fetch an URL and cache the response cache_age_days
 def cached_request(url, type, cache_age_days=1):
@@ -44,14 +45,16 @@ def cached_request(url, type, cache_age_days=1):
 # Get list of all Highres movies, popular right now, from piratebay
 def get_movies(start=0, stop=1):
     movies = []
-    print("Get pages (%s-%s): " % (start+1, stop))
+    print("Get pages (%s-%s): " % (start + 1, stop))
     for i in range(start, stop):
-        if not DEBUG: print(".", end="")
-        content = cached_request("http://thepiratebay.org/browse/207/%s/7" % i, "html")
+        if not DEBUG:
+            print(".", end="")
+        content = cached_request("http://thepiratebay.se/browse/207/%s/7" % i, "html")
         document = html.document_fromstring(content)
         links = document.cssselect(".detLink")
         movies.extend([(link.get("href"), link.text_content()) for link in links])
-    if not DEBUG: print()
+    if not DEBUG:
+        print()
     print()
     return movies
 
@@ -83,58 +86,13 @@ def get_grades(movies):
     grades = []
     print("Get grades (%s-%s): " % (1, len(movies)))
     for movie in movies:
-        if not DEBUG: print(".", end="")
+        if not DEBUG:
+            print(".", end="")
         grades.append(get_first(movie))
-    if not DEBUG: print()
+    if not DEBUG:
+        print()
     print()
     return set(grades)
-
-# Cut out the movie name from a torrent name
-def strip_title(title):
-    # Strings that mark the end of a movie name, and start of meta data
-    ends = [
-        "1080p", "720p", "bluray", "bdrip", "brrip", "hd",
-        "korsub", "extended", "uncut", "unrated", "repack",
-        "swesub", "ac3", "hdrip", "xvid", "dvdscr", "rc",
-        "hq", "boxset", "x264", "tc", "bdrip720p", "bdrip1080p",
-        "edition"
-    ]
-
-    # Remove all non-alpha characters
-    words = re.split(r"[\W_]+", title.lower())
-
-    # Loop over all words. As soon as a ending is found, cut to there
-    for i, word in enumerate(words):
-        if word in ends:
-            words = words[:i]
-            break
-    title = " ".join(words).lower()
-
-    # Remove unnessesary whitespace
-    return title.strip()
-
-# Reduce the number of duplicate movies by removing the year from name
-def unique(movies):
-    return set([("".join(re.split(" \d{4}$", title)).strip()) for title in movies])
-
-# Identify bundles of movies, as opposed to a single movie
-def is_bundle(title):
-    # Strings that identify a bundle
-    bundle_identifier = [
-        "trilogy", "duology", "quadrilogy",
-        "movies", "collection", "series", "complete",
-        " 1 3 ", " 1 4 ", " 1 5 "
-    ]
-    for identifier in bundle_identifier:
-        if identifier in title:
-            return True
-    return False
-
-# Separate movies, and movie bundles. Bundles will not be graded.
-def partition(mixed):
-    movies = [movie for movie in mixed if not is_bundle(movie)]
-    bundles = [movie for movie in mixed if is_bundle(movie)]
-    return movies, bundles
 
 # Print out debug messages, if run in verbose mode
 def debug(message):
@@ -142,35 +100,37 @@ def debug(message):
         print(message)
 
 def out(header, collection):
-    if not collection:
-        return
-
-    print(header)
-    print("-"*len(header))
-    if isinstance(collection[0], tuple):
-        max_len = min(60, len(max(collection, key=lambda x: len(x[2]))[2]))
-        for item in collection:
-            num = "%-2s" % item[0]
-            name = ("%-" + str(max_len + 1) + "s") % item[2]
-            link = item[3]
-            try:
-                print(num, name, link)
-            except UnicodeEncodeError:
-                print(num, name.encode("latin-1", link))
-    else:
-        for item in collection:
-            print(item)
-    print()
+    if collection:
+        print(header)
+        print("-" * len(header))
+        if isinstance(collection[0], tuple):
+            max_len = len(max(collection, key=lambda x: len(x[2]))[2])
+            for item in collection:
+                print("%-2s" % item[0], ("%-" + str(max_len + 1) + "s") % item[2], item[3])
+        else:
+            for item in collection:
+                print(item)
+        print()
 
 def main():
-    movies = unique([strip_title(title) for url, title in get_movies(stop=10)])
-    movies, bundles = partition(movies)
+    movies = get_movies(stop=PAGES)
+    movies = filenames_to_search_strings(movies)
+    movies, bundles = separate_movies_from_bundles(movies)
     graded = get_grades(movies)
 
     out("Movies not seen, by grade", sorted(filter(lambda x: x[1] != u'seen', graded), reverse=True))
     out("Bundles, not graded", sorted(bundles))
 
+# Possible calls:
+# parse.py --verbose
+# parse.py 10
+# parse.py --verbose 10
 if __name__ == "__main__":
-    if len(sys.argv) == 2 and sys.argv[1] == "--verbose":
-        DEBUG = True
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == "--verbose":
+            DEBUG = True
+        elif int(sys.argv[1]) > 0:
+            PAGES = int(sys.argv[1])
+        if len(sys.argv) == 3 and int(sys.argv[2]) > 0:
+            PAGES = int(sys.argv[2])
     main()
