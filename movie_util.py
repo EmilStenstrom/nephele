5a702d296e57c7855a668dab425ad02218699eba
Emil Stenström <em@kth.se>
2013-06-02 00:38:24 +0200

    Add more ending to cut from.

--- movie_util.py
+++ movie_util.py
@@ -4,9 +4,9 @@ import re
 # Cut out the movie name from a torrent name
 def movie_title_from_filename(title):
     # Strings that mark the end of a movie name, and start of meta data
-    ends = ["1280p", "1080p", "720p", "r6", "bluray", "bdrip", "brrip", "blu-ray", "blu", "bd", "hd", "hdtv", "hdcam", "korsub", "extended", "uncut", "unrated", "repack", "r3", "swesub", "ac3", "xvid", "hdrip", "dvdscr", "rc", "dvdrip", "dvdr", "webrip", "rerip", "proper", "hq", "directors", "retail", "boxset", "x264", "tc", "bdrip720p", "bdrip1080p", "edition", "limited", "french", "swedish", "hindi", "kor", "nlsubs", "pal", "mkv", "avi", "iso", "mp4", "mpeg", "mov"]
+    ends = ["1280p", "1080p", "720p", "r6", "bluray", "bdrip", "brrip", "blu-ray", "blu", "bd", "hd", "hdtv", "hdcam", "hdscr", "korsub", "extended", "uncut", "unrated", "repack", "r3", "swesub", "ac3", "xvid", "hdrip", "dvdscr", "rc", "dvdrip", "dvdr", "webrip", "rerip", "proper", "hq", "directors", "retail", "boxset", "x264", "tc", "bdrip720p", "bdrip1080p", "edition", "limited", "french", "swedish", "hindi", "kor", "nlsubs", "pal", "mkv", "avi", "iso", "mp4", "mpeg", "mov"]
     ends_i = ["iNTERNAL", "CUSTOM", "TS"]  # Case sensitive strings
-    ends_double = ["dir cut"]
+    ends_double = ["dir cut", "ext cut"]
     ends_triple = ["the final cut"]
 
     # Remove all non-alpha characters
