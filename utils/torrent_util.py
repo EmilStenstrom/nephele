05d4ddb90eae1270b6a31eadc6952e8dc4e665ac
Emil Stenström <em@kth.se>
2015-09-25 00:31:14 +0200

    Make sure cam filtering works again.

--- utils/torrent_util.py
+++ utils/torrent_util.py
@@ -12,9 +12,10 @@ def torrent_to_movie(name):
     name = re.sub(r"^((?:.*[ \.]){3,}\(\d\d\d\d\)).+", r"\1", name)
 
     # Remove all non-alpha characters
-    words = re.split(r"[^\w'\:]+", name, flags=re.UNICODE)
+    raw_words = re.split(r"[^\w'\:]+", name, flags=re.UNICODE)
 
     # Loop over all words. As soon as a ending is found, cut to there
+    words = raw_words
     for i, word in enumerate(words):
         if word.lower() in ends or word in ends_i or \
                 (i + 1 < len(words) and (word + " " + words[i + 1]).lower() in ends_double) or \
@@ -37,48 +38,53 @@ def torrent_to_movie(name):
     return {
         "name": name,
         "year": year,
+        "is_tv": is_tv(raw_words),
+        "is_cam": is_cam(raw_words),
+        "is_bundle": is_bundle(raw_words),
     }
 
+def is_tv(words):
+    tv_regex = r"s\d\de\d\d"
+    tokens = [word.lower() for word in words]
+
+    for word in tokens:
+        if re.search(tv_regex, word, re.IGNORECASE):
+            return True
+
+    if "hdtv" in tokens:
+        return True
+
+    return False
+
+# Ignore movies that are cams uploaded to the HD section
+def is_cam(words):
+    # Strings that identify a low quality movie
+    ignore_identifier = [
+        "cam", "dvdscr", "hc", "hdcam", "hdrip", "hdts", "hqscr", "korsub", "screener", "ts"
+    ]
+    tokens = [word.lower() for word in words]
+    for identifier in ignore_identifier:
+        if identifier in tokens:
+            return True
+
+    return False
+
+# Identify bundles of movies, as opposed to a single movie
+def is_bundle(words):
+    # Strings that identify a bundle
+    bundle_identifier = [
+        "trilogy", "duology", "quadrilogy",
+        "movies", "collection", "series", "complete",
+        " 1 3 ", " 1 4 ", " 1 5 ", " 1 2 3 "
+    ]
+    tokens = [word.lower() for word in words]
+    for identifier in bundle_identifier:
+        if identifier in tokens:
+            return True
+
+    return False
+
 def remove_bad_torrent_matches(movies):
-    # Identify bundles of movies, as opposed to a single movie
-    def is_bundle(name):
-        # Strings that identify a bundle
-        bundle_identifier = [
-            "trilogy", "duology", "quadrilogy",
-            "movies", "collection", "series", "complete",
-            " 1 3 ", " 1 4 ", " 1 5 ", " 1 2 3 "
-        ]
-        for identifier in bundle_identifier:
-            if identifier in name:
-                return True
-
-        return False
-
-    def ignore_tv_series(name):
-        tv_regex = r"s\d\de\d\d"
-        if re.search(tv_regex, name, re.IGNORECASE):
-            return True
-
-        if "hdtv" in name:
-            return True
-
-        return False
-
-    # Ignore movies that are cams uploaded to the HD section
-    def ignore_movies(name):
-        # Remove all non-alpha characters
-        words = re.split(r"[^\w'-:]+", name.lower())
-
-        # Strings that identify a low quality movie
-        ignore_identifier = [
-            "cam", "dvdscr", "hc", "hdcam", "hdrip", "hdts", "hqscr", "korsub", "screener", "ts"
-        ]
-        for identifier in ignore_identifier:
-            if identifier in words:
-                return True
-
-        return False
-
     def remove_duplicates_stable(movies):
         nodups = []
         for movie in movies:
@@ -87,9 +93,9 @@ def remove_bad_torrent_matches(movies):
 
         return nodups
 
-    movies = [movie for movie in movies if not is_bundle(movie["name"])]
-    movies = [movie for movie in movies if not ignore_movies(movie["name"])]
-    movies = [movie for movie in movies if not ignore_tv_series(movie["name"])]
+    movies = [movie for movie in movies if not movie["is_bundle"]]
+    movies = [movie for movie in movies if not movie["is_tv"]]
+    movies = [movie for movie in movies if not movie["is_cam"]]
     movies = remove_duplicates_stable(movies)
 
     return movies
