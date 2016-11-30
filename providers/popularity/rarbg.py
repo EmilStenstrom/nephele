62eebcc5864acb3519191c7dc4890d73222c0297
Emil Stenström <em@kth.se>
2016-11-30 20:55:38 +0100

    Add RARBG popularity provider.

---
+++ providers/popularity/rarbg.py
@@ -0,0 +1,28 @@
+from providers.popularity.provider import PopularityProvider
+from utils.torrent_util import torrent_to_movie, remove_bad_torrent_matches
+from urllib import urlencode
+
+IDENTIFIER = "rarbg"
+
+# API Documentation: https://torrentapi.org/apidocs_v2.txt
+
+class Provider(PopularityProvider):
+    def get_popular(self):
+        base = "https://torrentapi.org/pubapi_v2.php?"
+        data_token = self.parse_json(base + "get_token=get_token", cache=False)
+
+        params = {
+            "token": data_token["token"],
+            "mode": "list",
+            "sort": "seeders",
+            "category": "14;48;17;44;45;47;42;46",
+            "limit": 100,
+        }
+
+        url = base + urlencode(params)
+        data_movies = self.parse_json(url, cache=False)
+        names = [item["filename"] for item in data_movies["torrent_results"]]
+
+        movies = [torrent_to_movie(name) for name in names]
+        movies = remove_bad_torrent_matches(movies)
+        return movies
